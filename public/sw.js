0d41a8b2b5de2aa6dce6e5b1cb6830e111638a36
GiuseppeLeviBo <32899895+GiuseppeLeviBo@users.noreply.github.com>
2026-04-28 07:45:02 +0200

    Update CACHE_NAME to version 0.1.3

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "qix-style-v2";
+const CACHE_NAME = "qix-style-v0.1.3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
